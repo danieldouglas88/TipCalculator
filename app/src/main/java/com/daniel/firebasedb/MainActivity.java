8e7c25eda1b0ee7c95193b02fa447f193a1521c6
Daniel Douglas <Daniel.Douglas@seattlecolleges.edu>
2018-05-06 00:03:50 -0700

    Add new functionality and features

--- app/src/main/java/com/daniel/firebasedb/MainActivity.java
+++ app/src/main/java/com/daniel/firebasedb/MainActivity.java
@@ -11,12 +11,22 @@ import com.firebase.client.Firebase;
 import com.firebase.client.FirebaseError;
 import com.firebase.client.ValueEventListener;
 
+import java.time.Instant;
+import java.util.Random;
+
 public class MainActivity extends Activity {
 
     TextView textViewDisplay;
+    TextView textViewUn;
+    TextView textViewRandom;
     Button buttonSmall;
     Button buttonLarge;
     Firebase firebaseRef;
+    Firebase firebaseRefTwo;
+    Firebase firebaseRefThree;
+    Random rand = new Random();
+    String[] array = {"Earth", "Pluto", "Uranus", "Mars", "Neptune", "Jupiter", "Cosmos", "Milky Way", "Venus", "The Moon", "Black Hole"};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,11 +39,40 @@ public class MainActivity extends Activity {
         super.onStart();
 
         textViewDisplay = (TextView) findViewById(R.id.textViewDisplay);
+        textViewUn = (TextView) findViewById(R.id.textViewUn);
+        textViewRandom = (TextView) findViewById(R.id.textViewRandom);
         buttonLarge = (Button) findViewById(R.id.buttonLarge);
         buttonSmall = (Button) findViewById(R.id.buttonSmall);
         firebaseRef = new Firebase("https://db-app-6edfc.firebaseio.com/data/type");
+        firebaseRefTwo = new Firebase("https://db-app-6edfc.firebaseio.com/stuff");
+        firebaseRefThree = new Firebase("https://db-app-6edfc.firebaseio.com/random");
+
 
         firebaseRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String text = dataSnapshot.getValue(String.class);
+                textViewUn.setText(text);
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+            }
+        });
+
+        firebaseRefThree.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String text = dataSnapshot.getValue(String.class);
+                textViewRandom.setText(text);
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+            }
+        });
+
+        firebaseRefTwo.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 String text = dataSnapshot.getValue(String.class);
@@ -42,21 +81,32 @@ public class MainActivity extends Activity {
 
             @Override
             public void onCancelled(FirebaseError firebaseError) {
-
             }
         });
 
         buttonSmall.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
-                firebaseRef.setValue("Small");
+
+                firebaseRefTwo.setValue("Button Click Time: " + new java.util.Date());
+                int  n = rand.nextInt(1000000000) + 1;
+                int  i = rand.nextInt(array.length) + 0;
+                firebaseRef.setValue(array[i]);
+                firebaseRefThree.setValue("Random number: " + n);
+                buttonSmall.setText(array[i]);
+
             }
         });
 
         buttonLarge.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
-                firebaseRef.setValue("Large");
+                firebaseRefTwo.setValue("Button Click Time: " + new java.util.Date());
+                int  n = rand.nextInt(1000000000) + 1;
+                int  i = rand.nextInt(array.length) + 0;
+                firebaseRef.setValue(array[i]);
+                firebaseRefThree.setValue("Random number: " + n);
+                buttonLarge.setText(array[i]);
             }
         });
     }
